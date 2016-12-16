13c2b55fbab9cd621ef27569cc5c92137bad6bcd
Sasha Lebedinskiy <lebedinsky1987@gmail.com>
2016-12-16 20:32:15 +0400

    Update lesson4.sql

--- lesson4.sql
+++ lesson4.sql
@@ -4,9 +4,9 @@ select
   users.email,
   sum(order_sum.order_sum) as order_sum.total_sum,
 from 
-[crested-booking-152317:sqlsmysloi.e1_users] as users
-join [crested-booking-152317:sqlsmysloi.e1_orders] as order_sum
-on users.id=order_sum.user_id
+  [crested-booking-152317:sqlsmysloi.e1_users] as users
+  join [crested-booking-152317:sqlsmysloi.e1_orders] as order_sum
+  on users.id=order_sum.user_id
 group by users.first_name, 
   users.last_name, 
   users.email,
